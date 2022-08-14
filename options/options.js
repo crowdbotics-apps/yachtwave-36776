6387726c2c9cf3aa16302e438b3b24a54c59951e
Crowdbotics <team@crowdbotics.com>
2022-08-14 17:33:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "yachtwave_36776",
+  url: "https://yachtwave_36776.botics.co",
+  api: "https://yachtwave_36776.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
